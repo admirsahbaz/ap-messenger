6395b82569c8607b2c6e6482c61315407167260f
Elma Arslanagic <elma.arslanagic@apis-mac-6.local>
2017-01-24 00:23:44 +0100

    Layout changes

---
+++ APMessenger/SettingsViewController.h
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.h
+//  APMessenger
+//
+//  Created by Elma Arslanagic on 1/24/17.
+//  Copyright © 2017 Authority Partners. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UITableViewController
+
+@property (strong, nonatomic) NSString *identifier;
+
+@end
